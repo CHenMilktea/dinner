9fb7b8694dfc7053826e4a2475df3bdb4fd7085a
CHenMilktea <95963482+CHenMilktea@users.noreply.github.com>
2022-01-16 18:10:25 +0800

    Add files via upload

---
+++ qgis2web_2022_01_16-18_07_15_402951/layers/layers.js
@@ -0,0 +1,57 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var format__1 = new ol.format.GeoJSON();
+var features__1 = format__1.readFeatures(json__1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__1.addFeatures(features__1);
+var lyr__1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource__1, 
+                style: style__1,
+                interactive: true,
+                title: '<img src="styles/legend/_1.png" /> 套餐'
+            });
+var format__2 = new ol.format.GeoJSON();
+var features__2 = format__2.readFeatures(json__2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__2.addFeatures(features__2);
+var lyr__2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource__2, 
+                style: style__2,
+                interactive: true,
+                title: '<img src="styles/legend/_2.png" /> 和菜'
+            });
+var group_qgis = new ol.layer.Group({
+                                layers: [lyr_GoogleSatellite_0,lyr__1,lyr__2,],
+                                title: "qgis"});
+
+lyr_GoogleSatellite_0.setVisible(true);lyr__1.setVisible(true);lyr__2.setVisible(true);
+var layersList = [group_qgis];
+lyr__1.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__2.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__1.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr__2.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr__1.set('fieldLabels', {});
+lyr__2.set('fieldLabels', {});
+lyr__2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
